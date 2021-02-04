3501138c73b4271693f3a0cc24af95c656e1daf1
Coding Bird <Codingbird@yahoo.com>
2021-02-05 06:16:55 +1200

    captureId removed

--- script.js
+++ script.js
@@ -7,15 +7,12 @@ function getWeatherData(city) {
     .catch((err) => console.log(err));
 }
 
-//capture id
-const captureId = (i) => document.getElementById(i);
-
 function showData(weatherData) {
   if (!weatherData.message) {
     displayDataUpdate("city_name", weatherData.name);
     displayDataUpdate("temperature", `${weatherData.main.temp} °C`);
     displayDataUpdate("weather_mode_name", weatherData.weather[0].main);
-    captureId(
+    document.getElementById(
       "weather_mode_icon"
     ).src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
     console.log(weatherData);
@@ -23,17 +20,17 @@ function showData(weatherData) {
     displayDataUpdate("city_name", weatherData.message);
     displayDataUpdate("temperature", "");
     displayDataUpdate("weather_mode_name", "");
-    captureId("weather_mode_icon", "").src = "";
+    document.getElementById("weather_mode_icon", "").src = "";
   }
 }
 
 function displayDataUpdate(idName, value) {
-  captureId(idName).innerText = value;
+  document.getElementById(idName).innerText = value;
 }
 
-captureId("search_btn").addEventListener("click", () => {
+document.getElementById("search_btn").addEventListener("click", () => {
   var numberRegex = /^\s*[+-]?(\d+|\.\d+|\d+\.\d+|\d+\.)(e[+-]?\d+)?\s*$/;
-  const input = captureId("search_input");
+  const input = document.getElementById("search_input");
   if (!numberRegex.test(input.value)) {
     getWeatherData(input.value);
   } else alert("Please type a valid city name");
